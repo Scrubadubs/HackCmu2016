91b4030e0a80dccba653f8b1d8e0dc0c2394746b
Richard Gu <rgu1@andrew.cmu.edu>
2016-09-16 21:53:04 -0400

    added 2 view pages + the 1 controller for now

---
+++ flaskr/flaskr.py
@@ -0,0 +1,17 @@
+#import os
+from flask import Flask#, request, session, g, redirect, url_for, abort, \
+     #render_template, flash
+
+app = Flask(__name__)
+
+#app.config.from_object(__name__)
+
+
+
+#define views
+@app.route("/")
+def receivePage():
+	pass
+
+def inputPage():
+	pass
